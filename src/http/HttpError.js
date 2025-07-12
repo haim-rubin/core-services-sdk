9f2798057b61e59036493ea8f078cd26ddec90de
Haim Rubin <haim.rubin@gmail.com>
2025-07-12 11:44:31 +0300

    Fixed HttpError + added tests

--- src/http/HttpError.js
+++ src/http/HttpError.js
@@ -1,47 +1,74 @@
-import * as httpStatus from 'http-status'
+import httpStatus from 'http-status'
 
-const DEFAULT_ERROR = {
-  httpStatusCode: httpStatus.INTERNAL_SERVER_ERROR,
-  code: 'UNKNOWN',
-  httpStatusText: httpStatus[httpStatus.INTERNAL_SERVER_ERROR],
-}
-
-/**
- *
- */
 export class HttpError extends Error {
-  /** @type {number} */
-  httpStatusCode
-  /** @type {string} */
+  /** @type {string|undefined} */
   code
-  /** @type {string} */
+
+  /** @type {number|undefined} */
+  httpStatusCode
+
+  /** @type {string|undefined} */
   httpStatusText
 
-  /** @type {Array} */
-  details
-  constructor({
-    httpStatusCode,
-    code,
-    httpStatusText,
-    details = [],
-  } = DEFAULT_ERROR) {
-    super(`${code || httpStatus[httpStatusCode]}`)
-    this.httpStatusCode = httpStatusCode
+  /**
+   * @param {object} [error]
+   * @param {string} [error.code]
+   * @param {string} [error.message]
+   * @param {number} [error.httpStatusCode]
+   * @param {string} [error.httpStatusText]
+   */
+  constructor(error = {}) {
+    const { code, message, httpStatusCode, httpStatusText } = error
+
+    super(
+      message ||
+        (httpStatusCode && httpStatus[httpStatusCode]) ||
+        code ||
+        'Unknown error',
+    )
+
     this.code = code
-    this.httpStatusText = httpStatusText
-    this.details = details
+    this.httpStatusCode = httpStatusCode
+    this.httpStatusText =
+      httpStatusText || (httpStatusCode && httpStatus[httpStatusCode])
 
     if (typeof Error.captureStackTrace === 'function') {
       Error.captureStackTrace(this, this.constructor)
     }
   }
 
-  /**
-   *
-   * @param {object} instance
-   * @returns {boolean}
-   */
   static isInstanceOf(instance) {
     return instance instanceof HttpError
+  }
+
+  static [Symbol.hasInstance](instance) {
+    return (
+      instance &&
+      typeof instance === 'object' &&
+      'message' in instance &&
+      'httpStatusCode' in instance &&
+      'httpStatusText' in instance
+    )
+  }
+
+  toJSON() {
+    return {
+      code: this.code,
+      message: this.message,
+      httpStatusCode: this.httpStatusCode,
+      httpStatusText: this.httpStatusText,
+    }
+  }
+
+  static FromError(error) {
+    const httpError = new HttpError({
+      code: error.code || 'UNHANDLED_ERROR',
+      message: error.message || 'An unexpected error occurred',
+      httpStatusCode: httpStatus.INTERNAL_SERVER_ERROR,
+      httpStatusText: httpStatus[httpStatus.INTERNAL_SERVER_ERROR],
+    })
+
+    httpError.stack = error.stack
+    return httpError
   }
 }
