7565766dff98b5d5b89d8036cae4491cec9af4c4
Haim Rubin <haim.rubin@gmail.com>
2025-07-25 16:26:53 +0300

    Added jsDoc to transporter

--- src/mailer/transport.factory.js
+++ src/mailer/transport.factory.js
@@ -2,9 +2,33 @@ import aws from 'aws-sdk'
 import nodemailer from 'nodemailer'
 import sgTransport from 'nodemailer-sendgrid-transport'
 
+/**
+ * Factory for creating Nodemailer transporters based on configuration.
+ */
 export class TransportFactory {
   /**
-   * @param {object} config
+   * Create a Nodemailer transporter
+   *
+   * @param {object} config - Transport configuration object
+   * @param {'smtp' | 'gmail' | 'sendgrid' | 'ses'} config.type - Type of email transport
+   *
+   * For type 'smtp':
+   * @param {string} config.host - SMTP server host
+   * @param {number} config.port - SMTP server port
+   * @param {boolean} config.secure - Use TLS
+   * @param {{ user: string, pass: string }} config.auth - SMTP auth credentials
+   *
+   * For type 'gmail':
+   * @param {{ user: string, pass: string }} config.auth - Gmail credentials
+   *
+   * For type 'sendgrid':
+   * @param {string} config.apiKey - SendGrid API key
+   *
+   * For type 'ses':
+   * @param {string} config.accessKeyId - AWS access key
+   * @param {string} config.secretAccessKey - AWS secret
+   * @param {string} config.region - AWS region
+   *
    * @returns {import('nodemailer').Transporter}
    */
   static create(config) {
